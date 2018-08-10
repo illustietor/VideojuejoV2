e9c7dac5a3d700c3061805d75b43d5ee55445a4e
Kiwin00 <rene.18.krmm@gmail.com>
2018-08-11 00:50:41 +0200

    Juego adivinar terminado

--- Assets/Kevin/Minijuego Adivinar/Scripts/Adivinar.cs
+++ Assets/Kevin/Minijuego Adivinar/Scripts/Adivinar.cs
@@ -1,59 +1,137 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Adivinar : MonoBehaviour {
+public class Adivinar : MonoBehaviour
+{
+
     public Image imagen;
-    public Sprite[] imagenes;
+    public Sprite circulo, cuadrado, pentagono, peneKawaii, linea, triangulo;
+    Sprite[] imagenes = new Sprite[6];
 
     float tiempo;
+    public float tiempoTotal;
+
+    public int puntuacion;
 
     void AleatorizarImagenes()
     {
-
+        imagen.sprite = imagenes[Random.Range(0, imagenes.Length)];
     }
 
-    void ElegirOpciones()
+    void FinalJuego()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && imagenes[2]) //Elige la primera opcion (Triangulo)
+        if (tiempoTotal >= 20f)
         {
-            imagen.enabled = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.UpArrow) && imagenes[0]) //Elige la segunda opcion (Circulo)
-        {
-            imagen.enabled = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow) && imagenes[1]) //Elige la tercera opcion (Cuadrado)
-        {
-            imagen.enabled = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.DownArrow)) //Elige la cuarta opcion (Mierda Pura)
-        {
-            if (imagenes[3] || imagenes[4] || imagenes[5])
+            Time.timeScale = 0;
+            if (Input.GetKeyDown(KeyCode.R))
             {
-                imagen.enabled = false;
+                Scene scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.name);
+                Time.timeScale = 1;
             }
         }
     }
 
-    IEnumerator TiempoImagenes()
+    void ElegirOpciones()
     {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (imagen.sprite == triangulo) //Elige la primera opcion (Triangulo)
+            {
+                puntuacion++;
+                AleatorizarImagenes();
+                tiempo = 0;
+            }
+            else
+            {
+                puntuacion--;
+                AleatorizarImagenes();
+                tiempo = 0;
+            }
+        }
+        
 
-        yield return new WaitForSeconds(4);
-        imagen.sprite = imagenes[Random.Range(0, imagenes.Length)];
-        imagen.enabled = true;
-        yield return new WaitForSeconds(1);
+        if (Input.GetKeyDown(KeyCode.UpArrow)) //Elige la segunda opcion (Circulo)
+        {
+            if (imagen.sprite == circulo)
+            {
+                puntuacion++;
+                AleatorizarImagenes();
+                tiempo = 0;
+            }
+            else
+            {
+                puntuacion--;
+                AleatorizarImagenes();
+                tiempo = 0;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) //Elige la tercera opcion (Cuadrado)
+        {
+            if (imagen.sprite == cuadrado)
+            {
+                puntuacion++;
+                AleatorizarImagenes();
+                tiempo = 0;
+            }
+            else
+            {
+                puntuacion--;
+                AleatorizarImagenes();
+                tiempo = 0;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) //Elige la cuarta opcion (Mierda Pura)
+        {
+            if (imagen.sprite == linea || imagen.sprite == peneKawaii || imagen.sprite == pentagono)
+            {
+                puntuacion++;
+                AleatorizarImagenes();
+                tiempo = 0;
+            }
+            else
+            {
+                puntuacion--;
+                AleatorizarImagenes();
+                tiempo = 0;
+            }
+        }
     }
 
-	void Start () {
-		//Al inicio del juego se activa una imagen
-	}
-	
-	void Update () {
-            StartCoroutine(TiempoImagenes());
-	}
+
+    void Start()
+    {
+        //Al inicio del juego se activa una imagen
+
+        imagenes[0] = triangulo;
+        imagenes[1] = circulo;
+        imagenes[2] = cuadrado;
+        imagenes[3] = linea;
+        imagenes[4] = peneKawaii;
+        imagenes[5] = pentagono;
+    }
+
+    void Update()
+    {
+        tiempo += Time.deltaTime;
+        tiempoTotal += Time.deltaTime;
+
+        if (tiempo >= 3f)
+        {
+            AleatorizarImagenes();
+
+            tiempo = 0;
+        }
+        if (Time.timeScale == 1)
+        {
+            ElegirOpciones();
+        }
+
+        FinalJuego();
+    }
 }
