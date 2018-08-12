1a6c154ee4fd84cff5b92834118e4a438af4c1d4
illustietor <illustietor@gmail.com>
2018-08-12 10:41:49 +0200

    Subiendo parte de las cosillas que llevo del Minijuego de Programar

---
+++ Assets/Sergi/MJ - Programar 2.0/ScriptsMinijuegos/ScriptMjProgramarTutorial.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptMjProgramarTutorial : MonoBehaviour {
+
+    //¡Hola mundo!
+    //Este es el tutorial de programación.
+    //Aunque ya sepas programar te recomiendo echarle un vistazo.
+    //No te preocupes: no es muy largo y este programa es muy sencillo de usar.
+    //¡Pero tampoco te confies!
+    //Siguen haciendo falta mucha práctica y vocabulario para dominarlo.
+
+    //¿Que tal si empiezas escribiendome tu nombre?
+    string tuNombre = "Nombre";
+
+    
+    //Vaya, acabas de guardar tu nombre en una variable.
+    //¿Que qué leches es una variable?
+    //Pues las variables son como cajitas que guardan cosas.
+    //El problema es que cada cosa debe guardarse en un tipo de cajita concreta.
+
+    //Por un lado tenemos el STRING donde se guarda el texto, tambien conocido como conjunto de caracteres.
+    //Pero si no queremos meterlos todos en la misma caja siempre podemos guardarlos uno a uno en variables de tipo CHAR.
+    //Las variables de tipo CHAR son tan pequeñajas que solo pueden guardar un caracter.
+    //Recuerda que un caracter es cualquiera de los simbolos que puedes introducir con tu teclado.
+
+    //Por ejemplo, si has introducido tu nombre antes:
+    //Yo he preparado una caja tipo STRING con la etiqueta "tuNombre" para acordarme luego de que valor estoy guardando ahí.
+    //Y es que probablemente hayas introducido un conjunto de caracteres, es decir, un STRING.
+    //En cambio, si por lo general los nombres se compusieran por un solo caracter...
+    //Probablemente me hubiera sido mas eficiente guardar tu nombre en una variable tipo CHAR.
+
+    //¿Cual es la primera letra de tu apellido?
+    char tuLetra = 'L';
+
+
+    //Si durante el paso anterior te ha entrado curiosidad con lo que ya te contado y te has puesto a probar cosas con el teclado...
+    //Habrás podido comprobar que puedes introducir cualquier caracter de tu teclado.
+    //PUES CON LAS VARIABLES DE LOS NÚMEROS NO PUEDES HACER ESO.
+    int enteros = 0;
+    float decimales = 0.1F;
+
+
+    //Eso es, en las variables de INT y FLOAT solo puedes poner numericos.
+    //En las de tipo INT solo puedes introducir numeros enteros, es decir: nada de decimales.
+    //Para meter decimales tenemos las variables de tipo FLOAT.
+    //¿Que por qué hay una para cada cosa?
+    //Búscalo luego en Google que ahora no da tiempo.
+    //Lo importante es que sepas reconocer cada tipo de valor y asignarle el tipo correspondiente.
+    //Prueba estos ejercicios a ver si te has enterado bien:
+
+    string Ejercicio1 = "¿Que es esto?";
+    char Ejercicio2 = '?';
+    int Ejercicio3 = 69;
+    float Ejercicio4 = 3.14F;
+
+    //Si lo has hecho correctamente las palabras introducidas deberían ponerse de color amarillo.
+    //Si lo has hecho mal las palabras introducidas probablemente estén en rojo.
+
+    //En programación es importante introducir el codigo ¡SIN FALTAS!
+    //Si te equivocas introduciendo alguna letra el ordenador no sabrá como interpretar el texto...
+    //Y si el ordenador no interpreta bien el texto ya te digo yo que algo seguro fallará.
+    //Aunque peor es que te pases introduciendo números exagerados o escribiendo cosas que no tocan...
+
+    //Como programador no solo te tocará introducir texto si no tambien CORREGIR los errores de otros.
+    //Nadie es perfecto y cualquiera puede cometer fallos. No la pagues mucho contigo o con los demás.
+    //Menos mal que tienes este avanzadísimo software de programación que sabe detectar los errores mejor que nadie, ¿eh?
+    //Hasta soy capaz de romper las reglas lógica computacional con tal de hacerte la vida más fácil (y divertida).
+
+    //Recuerda que puedes acceder al Glosario para consultar dudas sobre el vocabulario de programación que no conozcas.
+    //¡Es importante que te lo acabes aprendiendo para poder programar cada vez más rápido!
+
+
+    //Y de regalo para terminar voy a explicarte qué son LOS BOOLEANOS.
+    //(Que nombre tan gracioso tienen, ¿eh? Booleanos. Boooooooooooole...anos. Je je).
+    //Los BOOLEANOS son variables que solo pueden guardar dos valores: "true" y "false".
+    //"True" significa "verdadero" y quiere decir que la respuesta a la pregunta es afirmativa.
+    //"False" significa "falso" y quiere decir que la respuesta a la pegunta es negativa.
+
+    //Por ejemplo:
+    bool sumaVerdadera = 2 + 2 == 4;
+
+    //En este caso concreto estamos preguntando si "2+2 (¿es igual a...?) 4".
+    //Como probablemente ya sepas si tienes un mínimo de conocimientos matemáticos...
+    //Sí: "2 + 2" es igual a "4", y por lo tanto "sumaVerdadera = true".
+    //¿Facil de entender no?
+    bool facilDeEntender = true;
+
+    //¡Pues ánimo y buena suerte!
+
+}
